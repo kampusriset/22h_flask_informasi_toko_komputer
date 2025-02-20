c0e9a0c568a488415d0d6d41431535c442a33bf2
Jonathanlingling <121177715+Jonathanlingling@users.noreply.github.com>
2025-02-20 23:16:36 +0700

    Update jonathan.py

--- Team/jonathan.py
+++ Team/jonathan.py
@@ -4,7 +4,7 @@ def perkenalan(nama, nim, peran):
     print("Peran saya yaitu : ", peran)
 
 if __name__ == "__main__":
-    nama = "David Jonathan Wijaya"
+    nama = "David jonathan Wijaya"
     nim = "22.130.10.469"
     peran = "Presentasi Review Website (Presenter)"
     perkenalan(nama, nim, peran)
